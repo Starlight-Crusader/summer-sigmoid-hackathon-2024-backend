01b69d19f5cf48082f5862aa37a2133c9cc64d76
AKalamaghin <arteom.kalamaghin@isa.utm.md>
2024-06-15 17:37:13 +0300

    Aaaa bunch of stuff and a function fore getting ratings by product name

--- ratings/views.py
+++ ratings/views.py
@@ -1,9 +1,32 @@
 from rest_framework import response, status
 from rest_framework.decorators import api_view
 from .models import Rating
-from .serializers import RatingSerializer
+from products.models import Product
+from .serializers import RatingSerializer, GetRatingSerializer
+
+
+def get_ratings_by_prod_name(prod_name):
+    product = Product.objects.get(name=prod_name)
+    prod_id = product.id
+
+    return Rating.objects.get(product=prod_id)
 
 
 @api_view(['POST'])
 def create_rating(request):
-    pass
+    serialzier = RatingSerializer(data=request.data)
+    serialzier.is_valid(raise_exception=True)
+
+    if serialzier.is_valid():
+        rating = serialzier.save()
+        rating_data = GetRatingSerializer(rating).data
+
+        return response.Response(
+            {'new_rating': rating_data},
+            status=status.HTTP_201_CREATED
+        )
+    else:
+        return response.Response(
+            {'message': serialzier.errors},
+            status=status.HTTP_400_BAD_REQUEST
+        )
--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('create', views.create_user),
     path('drop', views.delete_all_users),
     path('drop/<int:user_id>', views.delete_user_by_id),
+    path('inc/<int:user_id>', views.increment_value),
+    path('dec/<int:user_id>', views.decrement_value),
 ]
