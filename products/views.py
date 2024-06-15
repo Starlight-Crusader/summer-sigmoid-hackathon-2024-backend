3213524698fc53eefd7c577a03668a5797cccf15
AKalamaghin <arteom.kalamaghin@isa.utm.md>
2024-06-15 15:10:36 +0300

    Products create

--- products/views.py
+++ products/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework import response, status
+from .serializers import ProductSerializer
 
-# Create your views here.
+
+@api_view(['POST'])
+def create_product(request):
+    serializer = ProductSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+
+    if serializer.is_valid():
+        product = serializer.save()
+        product_data = ProductSerializer(product).data
+
+        return response.Response(
+            {'new_product': product_data},
+            status=status.HTTP_201_CREATED
+        )
+    else:
+        return response.Response(
+            {'message': serializer.errors},
+            status=status.HTTP_400_BAD_REQUEST
+        )
