277f0242d0a1713b73fa2d7f94398c71c9e4d32f
AKalamaghin <arteom.kalamaghin@isa.utm.md>
2024-06-15 18:36:06 +0300

    Merge conflict in ratings/urls.py fixed

--- ratings/urls.py
+++ ratings/urls.py
@@ -4,9 +4,5 @@ from . import views
 
 urlpatterns = [
     path('create', views.create_rating),
-<<<<<<< HEAD
-    path('reviews', views.get_reviews_by_name),
-=======
     path('get_by_pn', views.get_ratings_by_pn),
->>>>>>> 707db7fc320afc4fa404ec76235192db3db881d1
 ]   
