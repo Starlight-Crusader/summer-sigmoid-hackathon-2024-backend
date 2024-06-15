8cf65d141b4eb09c3670e9eb097d7ee03c049bc7
AKalamaghin <arteom.kalamaghin@isa.utm.md>
2024-06-15 13:57:12 +0300

    User POST view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import response, status
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+from serializers import UserSerialzier
+
+
+@api_view(['POST'])
+def create_user(request):
+    serializer = UserSerialzier(data=request.data)
+    serializer.is_valid(raise_exception=True)
+
+    if serializer.is_valid():
+        user = serializer.save()
+        user_data = UserSerializer(user).data
+
+        return response.Response(
+            {'user': user_data},
+            status=status.HTTP_201_CREATED
+        )
+    else:
+        return response.Response(
+            {'message': serializer.errors},
+            status=status.HTTP_400_BAD_REQUEST
+        )
