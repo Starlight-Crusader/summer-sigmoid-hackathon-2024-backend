a7ec81c5a251672da239a6e524ca9049e13aa739
AKalamaghin <arteom.kalamaghin@isa.utm.md>
2024-06-15 15:35:09 +0300

    Merge conflict resolved

--- categories/urls.py
+++ categories/urls.py
@@ -6,10 +6,6 @@ urlpatterns = [
     path('create', views.create_category),
     path('get_all', views.get_all_categories),
     path('get/<int:category_id>', views.get_category_by_id),
-<<<<<<< HEAD
     path('delete', views.delete_all_categories),
     path('delete/<int:category_id>', views.delete_category_by_id),
-]   
-=======
 ]
->>>>>>> f534889e37812deedb9dd3b9d7ea2e2f832127b7
