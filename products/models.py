bba10e93c618ea6c3c76a110d11cfb106d042678
AKalamaghin <arteom.kalamaghin@isa.utm.md>
2024-06-16 10:55:24 +0300

    URL as TextField-s

--- products/models.py
+++ products/models.py
@@ -5,6 +5,6 @@ from categories.models import Category
 class Product(models.Model):
     name = models.CharField(max_length=50, unique=True)
     image_url = models.CharField(max_length=200, null=True)
-    tinder_image_url = models.CharField(max_length=200, null=True)
+    tinder_image_url = models.TextField(null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.IntegerField(default=0)
