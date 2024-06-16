244153ac6cb69e62d7084d46ed93b574d4faa3e8
AKalamaghin <arteom.kalamaghin@isa.utm.md>
2024-06-16 05:50:14 +0300

    Prepared for deploy

--- app/settings.py
+++ app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import dj_database_url
 from pathlib import Path
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,18 +23,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bjd-b64em5@hdb4a&7idfpu0fihi^w!%3m+ijmxaznpj)6qup5'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "localhost:5173",
-    "127.0.0.1",
-    "127.0.0.1:5173"
-]
+# ALLOWED_HOSTS = [
+#     "localhost",
+#     "localhost:5173",
+#     "127.0.0.1",
+#     "127.0.0.1:5173"
+# ]
 
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -95,7 +97,8 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.parse("postgres://sigmoid_django_render_user:eC53JaX9WyJ5e4tLSH3GbLl0LsQZ2NVs@dpg-cpmm7cdds78s73ajldk0-a.oregon-postgres.render.com/sigmoid_django_render")
+database_url = os.environ.get("DATABASE_URL")
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 
 # Password validation
